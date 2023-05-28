6e94428c78cfcc52da9283630cbd16b15388e17d
alvinkli <alvinklj@gmail.com>
2023-05-28 09:11:29 +0000

    fixed accuracy_plot formatting

--- accuracy_plot.py
+++ accuracy_plot.py
@@ -16,26 +16,26 @@ models = []
 model_accuracies = []
 # Iterate through files in the folder
 for filename in os.listdir(folder_path):
-    file_path = os.path.join(folder_path, filename)
-    models.append(filename[:-4])
-    # Check if the path is a file
-    if os.path.isfile(file_path):
-        # Open the file for reading
-        with open(file_path, "r") as file:
-            accuracy = file.read()
-            model_accuracies.append(float(accuracy))
+  file_path = os.path.join(folder_path, filename)
+  models.append(filename[:-4])
+  # Check if the path is a file
+  if os.path.isfile(file_path):
+    # Open the file for reading
+    with open(file_path, "r") as file:
+      accuracy = file.read()
+      model_accuracies.append(float(accuracy))
 
 sns.set_style("whitegrid")
 def plot_all_models_accuracy_bar(models, model_accuracies):
-    x = np.arange(len(models))
-    plt.bar(x, model_accuracies)
-    plt.bar(x, models, align='center')
-    plt.xticks(x, models)
-    plt.xlabel('Model')
-    plt.ylabel('Accuracy')
-    plt.title("Model Prediction Accuracies")
-    plt.savefig(os.path.join("logs", "model_accuracies.pdf"))
-    plt.savefig(os.path.join("logs", "model_accuracies.png"))
-    plt.close()
+  x = np.arange(len(models))
+  plt.bar(models, model_accuracies)
+  plt.xlabel('Model')
+  plt.ylabel('Accuracy')
+  plt.title("Model Prediction Accuracies")
+  plt.xticks(rotation=60, ha = 'right')
+  plt.tight_layout()
+  plt.savefig(os.path.join("logs", "model_accuracies.pdf"))
+  plt.savefig(os.path.join("logs", "model_accuracies.png"))
+  plt.close()
 
 plot_all_models_accuracy_bar(models, model_accuracies)
