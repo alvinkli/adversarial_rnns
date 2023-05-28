1e462cd0d3d04456af452542e9eaa7a7f05547f7
alvinkli <alvinklj@gmail.com>
2023-05-28 01:39:58 -0400

    added sh file to run all test cases concurrently

---
+++ run_test_jobs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+GPU=0
+num_gpus=4
+
+# Loop through model types
+for model in LSTM LTC
+do
+
+  # Loop through training mode
+  for train_mode in adversarial nonadversarial
+  do
+
+    # Loop through testing mode
+    for test_mode in adversarial nonadversarial
+    do
+
+      # Loop through sampling mode
+      for sampling in regular irregular
+      do
+
+       echo $model $train_mode $test_mode $sampling
+       savedir="logs/${model}_${train_mode}_${test_mode}_${sampling}/model.ckpt"
+       CUDA_VISIBLE_DEVICES=$GPU python evaluate.py --path $savedir > ${savedir}/testlog 2>&1 &
+
+       # Increment GPU so we parallelize experiments on different gpus
+       GPU=$((GPU + 1))
+       GPU=$((GPU % num_gpus))
+       
+    done
+  done
+ done
+done
